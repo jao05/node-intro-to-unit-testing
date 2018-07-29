de11b7b9c020bf1009ed29538687bf52c59e818c
Jesse Outlaw <JesseOutlaw03@gmail.com>
2018-07-29 14:25:57 -0400

    second attempt at fizzBuzzer test

--- fizzBuzzer.js
+++ fizzBuzzer.js
@@ -1,3 +1,26 @@
+function fizzBuzzer(num) {
+	if (typeof num !== 'number') {
+		throw Error('`num` must be a number');
+	}
+	if (num % 15 === 0) {
+		return 'fizz-buzz';
+	}
+	if (num % 5 === 0) {
+		return 'buzz';
+	}
+	if (num % 3 === 0) {
+		return 'fizz';
+	}
+	else {
+		return num;
+	}
+}
+
+module.exports = fizzBuzzer;
+
+
+// Original code before changing 
+/*
 module.exports = function(num) {
 	if (typeof num !== 'number') {
 		throw Error('`num` must be a number');
@@ -15,3 +38,4 @@ module.exports = function(num) {
 		return num;
 	}
 }
+*/
--- test/test-fizzBuzzer.js
+++ test/test-fizzBuzzer.js
@@ -7,10 +7,51 @@ const adder = require('../fizzBuzzer');
 // unit tests for our `fizzBuzzer` function
 describe( 'fizzBuzzer', function()
 {
-	// test the normal case
-	it( 'should return the appropriate string OR num after modular division', function() {
+	// test the normal cases
+	// For multiples of 15
+	it( 'should return "fizz-buzz" for numbers that are divisible by 15', function() {
 
-		const normalCases = [ 'fizz', 'buzz', 'fizz-buzz', 1 ];
+		const normalCases = [ 15, 30, 45 ];
+
+		normalCases.forEach( function( input ) 
+		{
+			expect( fizzBuzzer( input ) ).to.equal( 'fizz-buzz' );
+		} );
+	} );
+
+	// For multiples of 5
+	it( 'should return "buzz" for numbers that are divisible by 5', function() {
+
+		// **********What if '15' was included here and the array above?****************
+		// ***** I assume it's okay that each "it" has its own 'normalCases'*************
+		const normalCases = [ 5, 10, 20 ];
+
+		normalCases.forEach( function( input ) 
+		{
+			expect( fizzBuzzer( input ) ).to.equal( 'buzz' );
+		} );
+	} );
+
+	// For multiples of 3
+	it( 'should return "fizz" for numbers that are divisible by 3', function() {
+
+		const normalCases = [ 3, 6, 9 ];
+
+		normalCases.forEach( function( input ) 
+		{
+			expect( fizzBuzzer( input ) ).to.equal( 'fizz' );
+		} );
+	} );
+
+	// For nums that are not multiples of 3 or 5 (and therefore 15)
+	it( 'should return the number if the number is not a multiple of 3 or 5', function() {
+
+		const normalCases = [ 1, 2, 4, 7 ];
+
+		normalCases.forEach( function( input ) 
+		{
+			expect( fizzBuzzer( input ) ).to.equal( input );
+		} );
 	} );
 
 	// test an edge case for instance,
